9282fb34dc2997c469285c6741af75fb9793295e
Lin Khant Htel <linkhanthtel@icloud.com>
2024-09-15 18:30:57 +0800

    Update booksCard.jsx

--- client/src/components/home/booksCard.jsx
+++ client/src/components/home/booksCard.jsx
@@ -3,7 +3,7 @@ import BookCard from './bookCard'
 
 function BooksCard({ books }) {
   return (
-    <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+    <div className='w-full pb-96 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
       {books.map((item) => (
         <BookCard key={item._id} book={item} />
       ))}
