09669d3de684d9df77aac8a79aed325d2d103e87
Lin Khant Htel <linkhanthtel@icloud.com>
2024-09-22 19:14:11 +0800

    Minor Update

--- api/config.js
+++ api/config.js
@@ -1,3 +1,4 @@
 export const PORT = 8888;
 
+// Input your password in <mongodbtesting>
 export const mongoDBURL = "mongodb+srv://linkhanthtel:mongodbtesting@cluster0.5u2my18.mongodb.net/books-collection?retryWrites=true&w=majority";
